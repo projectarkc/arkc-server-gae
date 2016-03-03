da293926e526f3edc5a07aa00915dac8474689e3
Noah <noah@arkc.org>
2016-03-03 11:50:06 +0800

    adapt addconn module

---
+++ addconn/reflect.go
@@ -0,0 +1,159 @@
+// A web app for Google App Engine that proxies HTTP requests and responses to a
+// Tor relay running meek-server.
+package reflect
+
+import (
+	"io"
+	"net/http"
+	"net"
+	"net/url"
+	"time"
+	"bufio"
+	//"log"
+	//"bytes"
+
+	"appengine"
+	"appengine/urlfetch"
+)
+
+const (
+		forwardURL = "http://home.arkc.org:3389/"
+	// A timeout of 0 means to use the App Engine default (5 seconds).
+	urlFetchTimeout = 20 * time.Second
+)
+
+var context appengine.Context
+//A very bad hack
+var forward string
+
+// Join two URL paths.
+func pathJoin(a, b string) string {
+	if len(a) > 0 && a[len(a)-1] == '/' {
+		a = a[:len(a)-1]
+	}
+	if len(b) == 0 || b[0] != '/' {
+		b = "/" + b
+	}
+	return a + b
+}
+
+// We reflect only a whitelisted set of header fields. In requests, the full
+// list includes things like User-Agent and X-Appengine-Country that the Tor
+// bridge doesn't need to know. In responses, there may be things like
+// Transfer-Encoding that interfere with App Engine's own hop-by-hop headers.
+var reflectedHeaderFields = []string{
+	"Content-Type",
+	"X-Session-Id",
+}
+
+// Get the original client IP address as a string. When using the standard
+// net/http server, Request.RemoteAddr is a "host:port" string; however App
+// Engine seems to use just "host". We check for both to be safe.
+func getClientAddr(r *http.Request) string {
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err == nil {
+		return host
+	}
+	return r.RemoteAddr
+}
+
+// Make a copy of r, with the URL being changed to be relative to forwardURL,
+// and including only the headers in reflectedHeaderFields.
+func processRequest() (forward string, payload io.Reader, sessionid string) {
+	if err != nil {
+		return nil, err
+	}
+	u, err := url.Parse(forward)
+	if err != nil {
+		return nil, err
+	}
+	// Append the requested path to the path in forwardURL, so that
+	// forwardURL can be something like "https://example.com/reflect".
+	//u.Path = pathJoin(u.Path, r.URL.Path)	
+	//log.Print("URL is " + u.String())
+	c, err := http.NewRequest("POST", u.String(), payload)
+	if err != nil {
+		return nil, err
+	}
+	c.Header.Add("X-Session-Id", sessionid)
+	return c, nil
+}
+
+func getauthstring(body bufio.Reader) (string, io.Reader, string, error) {
+	// TODO
+	//use datastore API and create what to send to the client
+	//return
+	// IP to send, string
+	// contents, io.Reader
+	// clientid, string
+	// password or authstring, string
+	// error
+}
+
+func authverify(body bufio.Reader, clientid string, authstring string) error{
+	// TODO
+	//verify if the password is correct
+}
+
+func storestring(clientid string, authstring string) (io.Reader, error) {
+	// TODO
+	//use Datastore and Memcache to store the string
+	//return
+	// current status, io.Reader
+	// error
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	context = appengine.NewContext(r)
+	forward, payload, clientid, mainpasswd, err := getauthstring(bufio.NewReader(r.Body))
+	if err != nil {
+		context.Errorf("parseRequest: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	sessionid = r.Header.Get("X-Session-Id")
+	fr, err := processRequest(forward, payload, sessionid)
+	if err != nil {
+		context.Errorf("processRequest: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Use urlfetch.Transport directly instead of urlfetch.Client because we
+	// want only a single HTTP transaction, not following redirects.
+	transport := urlfetch.Transport{
+		Context: context,
+		// Despite the name, Transport.Deadline is really a timeout and
+		// not an absolute deadline as used in the net package. In
+		// other words it is a time.Duration, not a time.Time.
+		Deadline: urlFetchTimeout,
+	}
+	resp, err := transport.RoundTrip(fr)
+	if err != nil {
+		context.Errorf("RoundTrip: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	err := authverify(resp.Body, clientid, mainpasswd)
+	if err != nil {
+		context.Errorf("Authentication: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	reply ,err := storestring(clientid, mainpasswd)
+	if err != nil {
+		context.Errorf("Saving: %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("X-Session-Id", sessionid)
+	w.WriteHeader(resp.StatusCode)
+	n, err := io.Copy(w, reply)
+	if err != nil {
+		context.Errorf("io.Copy after %d bytes: %s", n, err)
+	}
+}
+
+func init() {
+	http.HandleFunc("/", handler)
+}
